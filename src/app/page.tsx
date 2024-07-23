3800f13a6387018c249b01c7ac3455909897bdd8
Adeyemi Adekunle <adeyemisuraju15@gmail.com>
2024-07-23 10:43:42 +0100

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       </Section>
       <Section
         header='Application Launch Data'
-        // footer='These pages help developer to learn more about current launch information'
+        // footer='These pages help developer to learn more about current launch informatio'
       >
         <Link href='/init-data'>
           <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
